159504bc24dc5cda06c7827ae29c7d31211f8806
carolynnptkn <94360606+carolynnptkn@users.noreply.github.com>
2022-06-03 13:04:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Gửi babi Thỏ của em!',
+    introTitle: 'Gửi babi Hải cẩu mập của em!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
